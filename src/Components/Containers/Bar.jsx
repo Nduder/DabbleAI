b317621119570a10d11fbf22d72fc30410b616a1
Nicholas Chien <25749171+Nduder@users.noreply.github.com>
2023-02-07 01:00:38 -0800

    Revert "mvp"
    This reverts commit cbdb95d784fb61c901b1c2b3878d189e29bcc9d7.

--- src/Components/Containers/Bar.jsx
+++
@@ -1,9 +0,0 @@
-import { ApiChanger } from "../UtilityComponents/ChangeApi";
-
-export const Bar = () => {
-  return (
-    <div id="Bar">
-      <ApiChanger />
-    </div>
-  );
-};
--- src/Components/Features/Options/ImageGenOptions.jsx
+++
@@ -1,5 +0,0 @@
-import { useOptionsStore } from "../../../store/api";
-
-export const ImageGenOptions = () => {
-  return <div>its a div!</div>;
-};
